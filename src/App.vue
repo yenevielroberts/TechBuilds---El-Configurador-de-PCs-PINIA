62366f649add8476b426d27fcfb1917ae856d281
Yeneviel Roberts <yeneviel.roberts.7e8@itb.cat>
2026-01-22 16:49:46 +0100

    header, estilo, contenido

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
-<script setup></script>
+<script setup>
+import NavBar from './components/NavBar.vue';
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+ 
+ <NavBar/>
 </template>
 
-<style scoped></style>
+<style >
+
+  *{
+    margin: 0;
+    padding: 0;
+  }
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,46 @@
+<script setup>
+
+</script>
+
+<template>
+
+<header>
+    <div id="header-content">
+        <di>
+            <h1>TechBuilds</h1>
+        </di>
+        <div id="div-user-info">
+            <p>USerName</p>
+            <button>cart</button>
+        </div>
+    </div>
+</header>
+</template>
+
+<style scoped>
+
+header{
+    width: 100%;
+    background-color: cadetblue;
+    height: auto;
+}
+#header-content{
+    padding: 10px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+}
+
+#div-user-info{
+
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+}
+
+#div-user-info > button{
+    margin-left: 20px;
+   
+}
+</style>
